d00f77f8d455b38a3e5153a83d5962ab388cd6f0
Joe Kim <josephkim0224@gmail.com>
2017-03-21 15:00:45 -0400

    basic ui complete

---
+++ AudibleWalkthrough/PageCell.swift
@@ -0,0 +1,25 @@
+//
+//  PageCell.swift
+//  AudibleWalkthrough
+//
+//  Created by Joseph Kim on 3/21/17.
+//  Copyright © 2017 Joseph Kim. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: BaseCell {
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "page1")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override func setupViews() {
+        addSubview(imageView)
+        imageView.fillSuperview()
+    }
+}
--- AudibleWalkthrough/ViewController.swift
+++ AudibleWalkthrough/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    let cellId = "cellId"
+    
+    lazy var collectionView: UICollectionView = { [weak self] in
+        guard let this = self else {
+            return UICollectionView()
+        }
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        cv.delegate = this
+        cv.dataSource = this
+        cv.isPagingEnabled = true
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addSubview(collectionView)
+        collectionView.fillSuperview()
+        collectionView.register(PageCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PageCell
 
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
 
 }
 
